54758c54394bcdf51dd52965b523559b21376495
Brithany Lobo <brilobo04@gmail.com>
2025-08-24 11:38:28 -0600

    Creacion de seccion (Historial de Partidas)

--- src/main/java/ui/historialDePartidas/HistorialPanel.java
+++ src/main/java/ui/historialDePartidas/HistorialPanel.java
@@ -1,11 +1,21 @@
 package ui.historialDePartidas;
 
 import common.Interfaces.UIPanel;
+import service.PartidaService;
+
+import javax.swing.*;
+import java.awt.*;
 
 public class HistorialPanel extends UIPanel {
+    private PartidaService _partidasService = PartidaService.getInstance();
 
     @Override
     public void inicializar() {
-        
+        removeAll();
+        setLayout(new BorderLayout());
+
+        PartidasTableModel partidasTableModel = new PartidasTableModel(_partidasService.listar());
+        JTable partidasTable = new JTable(partidasTableModel);
+        add(new JScrollPane(partidasTable), BorderLayout.CENTER);
     }
 }
---
+++ src/main/java/ui/historialDePartidas/PartidasTableModel.java
@@ -0,0 +1,53 @@
+package ui.historialDePartidas;
+import model.Partida;
+
+import javax.swing.table.AbstractTableModel;
+import java.util.List;
+
+public class PartidasTableModel extends AbstractTableModel {
+    private String[] columns = { "ID", "Jugador", "Dificultad", "Tiempo", "Intentos", "Estado" };
+    private List<Partida> _partidaList;
+
+    public PartidasTableModel(List<Partida> partidaList) {
+        this._partidaList = partidaList;
+    }
+
+    public List<Partida> getPartidasList() {
+        return _partidaList;
+    }
+
+    public void setPartidasList(List<Partida> pacienteList) {
+        this._partidaList = pacienteList;
+        fireTableDataChanged();
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        return columns[column];
+    }
+
+    @Override
+    public int getRowCount() {
+        return _partidaList.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return columns.length;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Partida partida = _partidaList.get(rowIndex);
+
+        return switch(columnIndex){
+            case 0 -> partida.getId();
+            case 1 -> partida.getJugador().getNombre();
+            case 2 -> partida.getDificultad();
+            case 3 -> partida.getTiempo();
+            case 4 -> partida.getIntentos();
+            case 5 -> partida.isGanada() ? "Ganada" : "Perdida";
+            default -> null;
+        };
+    }
+}
